c3a7c6ffd5141ae57533a32d0dceba55669a716d
element54 <element54@posteo.de>
2017-11-07 11:42:48 +0100

    added my old cashier and customer

--- src/org/awesomebakery/agents/Customer.java
+++ src/org/awesomebakery/agents/Customer.java
@@ -1,31 +1,117 @@
 package org.awesomebakery.agents;
 
-import org.awesomebakery.model.Location;
+import java.util.List;
+import java.util.Vector;
 
-public class Customer {
+import jade.core.AID;
+import jade.core.Agent;
+import jade.core.behaviours.Behaviour;
+import jade.core.behaviours.OneShotBehaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
 
-	private String id;
-	private Location location;
+public class Customer extends Agent {
 
-	public Customer(String id, Location location) {
-		this.id = id;
-		this.location = location;
+	private static final long serialVersionUID = 1L;
+	private List<AID> cashiers = new Vector<>();
+
+	protected void setup() {
+		DFAgentDescription dfd = new DFAgentDescription();
+		dfd.setName(getAID());
+		ServiceDescription sd = new ServiceDescription();
+		sd.setType("customer");
+		sd.setName("customer");
+		dfd.addServices(sd);
+		try {
+			DFService.register(this, dfd);
+		} catch (FIPAException e) {
+			// TODO handle
+			e.printStackTrace();
+		}
+		addBehaviour(new FindCashiersBehaviour());
 	}
 
-	public String getId() {
-		return id;
+	private class FindCashiersBehaviour extends Behaviour {
+		private static final long serialVersionUID = 1L;
+
+		@Override
+		public void action() {
+			DFAgentDescription template = new DFAgentDescription();
+			ServiceDescription sd = new ServiceDescription();
+			sd.setType("cashier");
+			template.addServices(sd);
+			try {
+				cashiers.clear();
+				DFAgentDescription[] result = DFService.search(myAgent, template);
+				for (DFAgentDescription d : result) {
+					cashiers.add(d.getName());
+				}
+			} catch (FIPAException fe) {
+				fe.printStackTrace();
+			}
+			if (!cashiers.isEmpty()) {
+				myAgent.addBehaviour(new PlaceOrderBehaviour());
+			}
+
+		}
+
+		@Override
+		public boolean done() {
+			// TODO Auto-generated method stub
+			return !cashiers.isEmpty();
+		}
+
 	}
 
-	public void setId(String id) {
-		this.id = id;
+	private MessageTemplate mt = null;
+
+	private class PlaceOrderBehaviour extends OneShotBehaviour {
+		private static final long serialVersionUID = 1L;
+
+		@Override
+		public void action() {
+			ACLMessage cfp = new ACLMessage(ACLMessage.CFP);
+			cfp.addReceiver(cashiers.get(0));
+			cfp.setContent("test1");
+			cfp.setConversationId("order");
+			cfp.setReplyWith("cfp" + System.currentTimeMillis());
+			myAgent.send(cfp);
+			mt = MessageTemplate.and(MessageTemplate.MatchConversationId(cfp.getConversationId()),
+					MessageTemplate.MatchInReplyTo(cfp.getReplyWith()));
+			myAgent.addBehaviour(new CheckResponseBehaviour());
+		}
+
 	}
 
-	public Location getLocation() {
-		return location;
-	}
+	private class CheckResponseBehaviour extends Behaviour {
+		private static final long serialVersionUID = 1L;
+		boolean done = false;
+		@Override
+		public void action() {
+			ACLMessage reply = myAgent.receive(mt);
+			if (reply != null) {
+				if (reply.getPerformative() == ACLMessage.ACCEPT_PROPOSAL) {
+					// Good, nothing to do. exit
+					myAgent.doDelete();
+				} else {
+					// Failed, retry next time.
+					System.out.println("failed");
+					myAgent.addBehaviour(new FindCashiersBehaviour());
+					done = true;
+				}
+			} else {
+				block();
+			}
+		}
 
-	public void setLocation(Location location) {
-		this.location = location;
-	}
+		@Override
+		public boolean done() {
+			return done;
+		}
 
+	}
 }
