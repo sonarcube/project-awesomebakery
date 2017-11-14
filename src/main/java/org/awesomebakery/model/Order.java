6a0f852a9b2965a0ab4da3fa1bf3420675ecb853
Markus Michael Wiktorin <markus.wiktorin@smail.inf.h-brs.de>
2017-11-14 15:54:40 +0100

    add products to order parsing

--- src/main/java/org/awesomebakery/model/Order.java
+++ src/main/java/org/awesomebakery/model/Order.java
@@ -1,5 +1,9 @@
 package org.awesomebakery.model;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 import com.fasterxml.jackson.databind.JsonNode;
 
 public class Order {
@@ -7,13 +11,20 @@ public class Order {
 	private String customerId;
 	private Date orderDate;
 	private Date deliveryDate;
-	private int[] orders;
+	private Map<String, Integer> products;
 
 	public static Order fromJson(JsonNode node) {
 		Order order = new Order();
 		order.customerId = node.get("customer-id").asText();
 		order.deliveryDate = Date.fromJson(node.get("delivery-date"));
 		order.orderDate = Date.fromJson(node.get("order-date"));
+		order.products = new HashMap<>();
+		JsonNode products = node.get("products");
+		Iterator<String> productIterator = products.fieldNames();
+		while (productIterator.hasNext()) {
+			String productName = productIterator.next();
+			order.products.put(productName, products.get(productName).asInt());
+		}
 		return order;
 	}
 
@@ -29,8 +40,8 @@ public class Order {
 		return deliveryDate;
 	}
 
-	public int[] getOders() {
-		return orders;
+	public Map<String, Integer> getProducts() {
+		return products;
 	}
 
 }
