7fd3f3d25b70778e2bcf506eca46ca3a6ee5e6d1
Markus Michael Wiktorin <markus.wiktorin@smail.inf.h-brs.de>
2017-11-16 23:41:02 +0100

    adapted startup and added simple order transmission

--- src/main/java/org/awesomebakery/Start.java
+++ src/main/java/org/awesomebakery/Start.java
@@ -1,17 +1,27 @@
 package org.awesomebakery;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.awesomebakery.agents.Bakery;
 import org.awesomebakery.agents.Customer;
-import org.awesomebakery.agents.Factory;
+import org.awesomebakery.model.Order;
+import org.awesomebakery.model.Scenario;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import jade.core.Profile;
 import jade.core.ProfileImpl;
 import jade.util.ExtendedProperties;
 import jade.util.leap.Properties;
 import jade.wrapper.AgentContainer;
-import jade.wrapper.ControllerException;
 
 public class Start {
-	public static void main(String[] args) throws ControllerException {
+	public static void main(String[] args) throws Throwable {
 		jade.core.Runtime runtime = jade.core.Runtime.instance();
 		runtime.setCloseVM(true);
 		Properties profileProperties = new ExtendedProperties();
@@ -19,8 +29,37 @@ public class Start {
 		profileProperties.put(Profile.GUI, false);
 		Profile profile = new ProfileImpl(profileProperties);
 		AgentContainer mainContainer = runtime.createMainContainer(profile);
-		mainContainer.acceptNewAgent("bakery", new Factory()).start();
-		mainContainer.acceptNewAgent("customer", new Customer()).start();
+		
+		File file = new File("src/main/config/sample-scenario.json");
+		ObjectMapper objectMapper = new ObjectMapper();
+		JsonNode node;
+		try {
+			node = objectMapper.readTree(file);
+		} catch (Throwable t) {
+			System.err.println("Failed to read Scenario file " + file.getAbsolutePath());
+			throw t;
+		}
+
+		Scenario scenario = Scenario.fromJson(node);
+		
+		Map<String, List<Order>> customerOrders = new HashMap<>();
+		for (Order order : scenario.getOrders()) {
+			if (!customerOrders.containsKey(order.getCustomerId())) {
+				customerOrders.put(order.getCustomerId(), new ArrayList<Order>());
+			}
+			customerOrders.get(order.getCustomerId()).add(order);
+		}
+
+		for (org.awesomebakery.model.Customer customer : scenario.getCustomers()) {
+			Customer agent = new Customer(customerOrders.get(customer.getName()));
+			mainContainer.acceptNewAgent(customer.getName(), agent).start();
+		}
+		
+		for(org.awesomebakery.model.Bakery bakery : scenario.getBakeries()) {
+			Bakery agent = new Bakery(bakery.getName());
+			mainContainer.acceptNewAgent(bakery.getName(), agent).start();
+		}
+		
 		mainContainer.start();
 	}
 }
--- src/main/java/org/awesomebakery/agents/Customer.java
+++ src/main/java/org/awesomebakery/agents/Customer.java
@@ -8,6 +8,8 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.Vector;
 
+import org.awesomebakery.model.Order;
+
 import jade.core.AID;
 import jade.core.Agent;
 import jade.core.behaviours.Behaviour;
@@ -21,125 +23,110 @@ import jade.lang.acl.MessageTemplate;
 
 public class Customer extends Agent {
 
-    private static final long serialVersionUID = 1L;
-    private List<AID> factories = new Vector<>();
-    private static final String customer = "customer";
-    private static final String typeFactory = "Factory";
-    private static final String seperator = ",";
-    private static final String filepath = "./test.csv";
+	private final List<Order> orders;
+	private List<AID> bakeries;
 
-    private List schedulOrder(String seperator, String filepath) throws FileNotFoundException {
-        ArrayList<String> scheduleList = new ArrayList<String>();
-        Scanner scanner = new Scanner(new File(filepath));
-        scanner.useDelimiter(seperator);
-        while (scanner.hasNext()) {
-            scheduleList.add(scanner.next());
-        }
-        scanner.close();
-        return scheduleList;
-    }
+	public Customer(List<Order> orders) {
+		this.orders = orders;
+	}
 
+	protected void setup() {
+		if (orders != null && !orders.isEmpty()) {
+			addBehaviour(new FindBakeryBehaviour());
+		}
+	}
 
-    protected void setup() {
-        DFAgentDescription dfd = new DFAgentDescription();
-        dfd.setName(getAID());
-        ServiceDescription sd = new ServiceDescription();
-        sd.setType(customer);
-        sd.setName(customer);
-        dfd.addServices(sd);
-        try {
-            DFService.register(this, dfd);
-        } catch (FIPAException e) {
-            // TODO handle
-            e.printStackTrace();
-        }
-        addBehaviour(new FindBakeryBehaviour());
-    }
+	private class FindBakeryBehaviour extends Behaviour {
+		private static final long serialVersionUID = 1L;
 
-    private class FindBakeryBehaviour extends Behaviour {
-        private static final long serialVersionUID = 1L;
+		@Override
+		public void action() {
+			DFAgentDescription template = new DFAgentDescription();
+			ServiceDescription sd = new ServiceDescription();
+			sd.setType(Bakery.SERVICE_TYPE);
+			template.addServices(sd);
+			if (bakeries == null) {
+				bakeries = new ArrayList<>();
+			}
+			try {
+				DFAgentDescription[] result = DFService.search(myAgent, template);
+				for (DFAgentDescription d : result) {
+					bakeries.add(d.getName());
+				}
+			} catch (FIPAException fe) {
+				fe.printStackTrace();
+			}
+			if (!bakeries.isEmpty()) {
+				for (Order order : orders) {
+					myAgent.addBehaviour(new PlaceOrderBehaviour(order));
+				}
+			}
 
-        @Override
-        public void action() {
-            DFAgentDescription template = new DFAgentDescription();
-            ServiceDescription sd = new ServiceDescription();
-            sd.setType(typeFactory);
-            template.addServices(sd);
-            try {
-                factories.clear();
-                DFAgentDescription[] result = DFService.search(myAgent, template);
-                for (DFAgentDescription d : result) {
-                    factories.add(d.getName());
-                }
-            } catch (FIPAException fe) {
-                fe.printStackTrace();
-            }
-            if (!factories.isEmpty()) {
-                myAgent.addBehaviour(new PlaceOrderBehaviour());
-            }
+		}
 
-        }
+		@Override
+		public boolean done() {
+			return !bakeries.isEmpty();
+		}
 
-        @Override
-        public boolean done() {
-            // TODO Auto-generated method stub
-            return !factories.isEmpty();
-        }
+	}
 
-    }
+	private MessageTemplate mt = null;
 
-    private MessageTemplate mt = null;
+	private class PlaceOrderBehaviour extends OneShotBehaviour {
+		private static final long serialVersionUID = 1L;
 
-    private class PlaceOrderBehaviour extends OneShotBehaviour {
-        private static final long serialVersionUID = 1L;
+		private Order order;
 
-        @Override
-        public void action() {
-            ACLMessage cfp = new ACLMessage(ACLMessage.CFP);
-            cfp.addReceiver(factories.get(0));
-            try {
-                cfp.setContent(String.valueOf(schedulOrder(seperator, filepath)));
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            }
-            cfp.setConversationId("order");
-            cfp.setReplyWith("cfp" + System.currentTimeMillis());
-            myAgent.send(cfp);
-            mt = MessageTemplate.and(MessageTemplate.MatchConversationId(cfp.getConversationId()),
-                    MessageTemplate.MatchInReplyTo(cfp.getReplyWith()));
-            myAgent.addBehaviour(new CheckResponseBehaviour());
-        }
+		public PlaceOrderBehaviour(Order order) {
+			this.order = order;
+		}
 
-    }
+		@Override
+		public void action() {
+			ACLMessage cfp = new ACLMessage(ACLMessage.CFP);
+			for (AID bakery : bakeries) {
+				cfp.addReceiver(bakery);
+			}
+			cfp.setContent(order.toString());
+			cfp.setConversationId("request-price-for" + order.getGuid());
+			cfp.setReplyWith("cfp" + System.currentTimeMillis());
+			myAgent.send(cfp);
+			mt = MessageTemplate.and(MessageTemplate.MatchConversationId(cfp.getConversationId()),
+					MessageTemplate.MatchInReplyTo(cfp.getReplyWith()));
+			myAgent.addBehaviour(new CheckResponseBehaviour());
+		}
 
-    private class CheckResponseBehaviour extends Behaviour {
-        private static final long serialVersionUID = 1L;
-        boolean done = false;
+	}
 
-        @Override
-        public void action() {
-            ACLMessage reply = myAgent.receive(mt);
-            if (reply != null) {
-                if (reply.getPerformative() == ACLMessage.ACCEPT_PROPOSAL) {
-                    // Good, nothing to do. exit
-                    myAgent.doDelete();
-                } else {
-                    // Failed, retry next time.
-                    System.out.println("failed");
-                    myAgent.addBehaviour(new FindBakeryBehaviour());
-                    if (done()) {
-                        myAgent.doDelete();
-                    }
-                }
-            } else {
-                block();
-            }
-        }
+	private class CheckResponseBehaviour extends Behaviour {
+		private static final long serialVersionUID = 1L;
+		boolean done = false;
 
-        @Override
-        public boolean done() {
-            return done = true;
-        }
+		@Override
+		public void action() {
+			ACLMessage reply = myAgent.receive(mt);
+			if (reply != null) {
+				if (reply.getPerformative() == ACLMessage.ACCEPT_PROPOSAL) {
+					// Good, nothing to do. exit
+					myAgent.doDelete();
+				} else {
+					// Failed, retry next time.
+					System.out.println("failed");
+					myAgent.addBehaviour(new FindBakeryBehaviour());
+					if (done()) {
+						myAgent.doDelete();
+					}
+				}
+			} else {
+				block();
+			}
+		}
 
-    }
+		@Override
+		public boolean done() {
+			return done = true;
+		}
+
+	}
 }
