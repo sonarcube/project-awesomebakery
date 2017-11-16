fabb24a7058a939b4579dee3e260e12edcd95356
element54 <element54@posteo.de>
2017-11-16 21:49:21 +0100

    launch agents using java code

--- src/main/java/org/awesomebakery/Start.java
+++ src/main/java/org/awesomebakery/Start.java
@@ -1,24 +1,26 @@
 package org.awesomebakery;
 
-import java.util.List;
-import java.util.Vector;
+import org.awesomebakery.agents.Customer;
+import org.awesomebakery.agents.Factory;
+
+import jade.core.Profile;
+import jade.core.ProfileImpl;
+import jade.util.ExtendedProperties;
+import jade.util.leap.Properties;
+import jade.wrapper.AgentContainer;
+import jade.wrapper.ControllerException;
 
 public class Start {
-    public static void main(String[] args) {
-
-    	List<String> agents = new Vector<>();
-    	agents.add("bakery:org.awesomebakery.agents.Factory");
-    	agents.add("customer:org.awesomebakery.agents.Customer");
-
-
-    	List<String> cmd = new Vector<>();
-    	cmd.add("-agents");
-    	StringBuilder sb = new StringBuilder();
-    	for (String a : agents) {
-    		sb.append(a);
-    		sb.append(";");
-    	}
-    	cmd.add(sb.toString());
-        jade.Boot.main(cmd.toArray(new String[cmd.size()]));
-    }
+	public static void main(String[] args) throws ControllerException {
+		jade.core.Runtime runtime = jade.core.Runtime.instance();
+		runtime.setCloseVM(true);
+		Properties profileProperties = new ExtendedProperties();
+		profileProperties.put(Profile.MAIN, true);
+		profileProperties.put(Profile.GUI, false);
+		Profile profile = new ProfileImpl(profileProperties);
+		AgentContainer mainContainer = runtime.createMainContainer(profile);
+		mainContainer.acceptNewAgent("bakery", new Factory()).start();
+		mainContainer.acceptNewAgent("customer", new Customer()).start();
+		mainContainer.start();
+	}
 }
