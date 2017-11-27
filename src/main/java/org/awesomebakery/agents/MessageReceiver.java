bdbf396069f9f9039af11d7996e68cc74405b29b
Markus Michael Wiktorin <markus.wiktorin@smail.inf.h-brs.de>
2017-11-27 14:25:00 +0100

    make messagReceiver serializable to avoid sonarcube error

--- src/main/java/org/awesomebakery/agents/MessageReceiver.java
+++ src/main/java/org/awesomebakery/agents/MessageReceiver.java
@@ -1,7 +1,8 @@
 package org.awesomebakery.agents;
 
 import jade.lang.acl.ACLMessage;
+import jade.util.leap.Serializable;
 
-public interface MessageReceiver {
+public interface MessageReceiver extends Serializable {
 	public void onMessageReceived(ACLMessage message);
 }
